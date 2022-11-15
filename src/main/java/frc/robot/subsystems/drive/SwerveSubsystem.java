9c9eec5f2c0dd0a12af09571f582e313b712cdf7
person4268 <28717044+person4268@users.noreply.github.com>
2022-11-15 11:03:15 -0500

    remove unused import, pt 2

--- src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
