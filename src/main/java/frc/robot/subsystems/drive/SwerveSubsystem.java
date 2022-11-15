b13c60998e955c445a96c358545a57532e09e469
person4268 <28717044+person4268@users.noreply.github.com>
2022-11-15 09:44:48 -0500

    Removed unused import

--- src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems.drive;
 
-import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
