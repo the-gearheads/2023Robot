0625ed378279316b6d0f8a383a9d399270a41f68
person4268 <28717044+person4268@users.noreply.github.com>
2023-01-27 11:18:17 -0500

    Remove unused functions in vision.java

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -5,22 +5,18 @@
 package frc.robot.subsystems.vision;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -71,66 +67,4 @@ public class Vision extends SubsystemBase {
   @Override
   public void periodic() {
   }
-
-  private double calculateDifference(Pose3d x, Pose3d y) {
-    return x.getTranslation().getDistance(y.getTranslation());
-  }
-
-  private Pair<Pose3d, Double> closestToReferencePoseStrategy(Pose2d referencePose, double servoAngle) {
-    //Create Cam List
-    Translation3d camTrans = Constants.Vision.robotToCam.getTranslation();
-    Rotation3d servoAngleRot3d = new Rotation3d(0,0,servoAngle);
-    Rotation3d camRot = Constants.Vision.robotToCam.getRotation().rotateBy(servoAngleRot3d);
-    Transform3d robotToCam = new Transform3d(camTrans, camRot);
-
-    var cameras = new ArrayList<Pair<PhotonCamera, Transform3d>>();
-    cameras.add(new Pair<PhotonCamera, Transform3d>(targetCam, robotToCam));
-
-    if (referencePose == null) {
-        DriverStation.reportError(
-                "[PhotonPoseEstimator] Tried to use reference pose strategy without setting the reference!",
-                false);
-        return null;
-    }
-    double smallestDifference = 10e9;
-    double latency = 0;
-    Pose3d pose = null;
-    double angle = 0;
-    for (int i = 0; i < cameras.size(); i++) {
-        Pair<PhotonCamera, Transform3d> p = cameras.get(i);
-        var result = p.getFirst().getLatestResult();
-        List<PhotonTrackedTarget> targets = result.targets;
-        for (int j = 0; j < targets.size(); j++) {
-            PhotonTrackedTarget target = targets.get(j);
-            Optional<Pose3d> fiducialPose = Constants.Vision.atfl.getTagPose(target.getFiducialId());
-            if (fiducialPose.isEmpty()) {
-                continue;
-            }
-            Pose3d targetPose = fiducialPose.get();
-            Pose3d botBestPose =
-                    targetPose
-                            .transformBy(target.getAlternateCameraToTarget().inverse())
-                            .transformBy(p.getSecond().inverse());
-            Pose3d botAltPose =
-                    targetPose
-                            .transformBy(target.getBestCameraToTarget().inverse())
-                            .transformBy(p.getSecond().inverse());
-            double alternativeDifference = Math.abs(calculateDifference(new Pose3d(referencePose), botAltPose));
-            double bestDifference = Math.abs(calculateDifference(new Pose3d(referencePose), botBestPose));
-            if (alternativeDifference < smallestDifference) {
-                smallestDifference = alternativeDifference;
-                pose = botAltPose;
-                latency = result.getLatencyMillis();
-                angle=target.getBestCameraToTarget().getRotation().getZ();
-            }
-            if (bestDifference < smallestDifference) {
-                smallestDifference = bestDifference;
-                pose = botBestPose;
-                latency = result.getLatencyMillis();
-                angle=target.getBestCameraToTarget().getRotation().getZ();
-            }
-        }
-    }
-    return Pair.of(pose, latency);
-}
 }
