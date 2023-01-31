91edc95afd06a0453785112be0a0db2689e31268
person4268 <28717044+person4268@users.noreply.github.com>
2023-01-31 08:59:54 -0500

    basic implementation of auton stuff

---
+++ src/main/java/frc/robot/commands/auton/actions/PrintStringAction.java
@@ -0,0 +1,20 @@
+package frc.robot.commands.auton.actions;
+
+public class PrintStringAction extends AutonAction {
+  String tp;
+  public PrintStringAction(String toPrint) {
+    super();
+    tp = toPrint;
+  }
+
+  @Override
+  public void execute() {
+    System.out.println(tp);
+    this.cancel();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
