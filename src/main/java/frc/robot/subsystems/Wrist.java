874533fcd494bebb6ad4633f5f65e8528e160400
AnotherUser12 <150020151@gpschools.org>
2023-02-09 20:19:35 -0500

    wrist exists (disclaimer: will not work on actual wrist or mythical wrist)

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -4,32 +4,38 @@
 
 package frc.robot.subsystems;
 
-import java.util.ResourceBundle.Control;
-import org.opencv.core.Mat;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
-
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Arm extends SubsystemBase {
-  /** Creates a new arm. */
-  private double goal = 0;
+public class Wrist extends SubsystemBase {
+  /** Creates a new Wrist. */
+  private double goal;
   private CANSparkMax motor = new CANSparkMax(0, null);
   private SparkMaxAbsoluteEncoder encoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
   private PIDController pid = new PIDController(0, 0, 0);
   private ArmFeedforward ff = new ArmFeedforward(0, 0, 0);
+  private Arm arm;
 
-  public enum ControlMode{
-    VEL, POS;
+  public enum WristState{//assuming arm pointing completely upwards is 0 degrees; increasing counterclockwise; range [-pi, pi]
+    UP(-90, 60), LINEAR(61, 110), FORWARD(-91, 109);
+    private double max;
+    private double min;
+
+    private WristState(double min, double max){
+      this.min=min;
+      this.max=max;
+    }
+
   }
-  public ControlMode controlMode;
-  
-  public Arm() {
-    controlMode=ControlMode.VEL;
+
+  public Wrist(Arm arm) {
+    this.arm = arm;
     configure();
   }
 
@@ -53,26 +59,23 @@ public class Arm extends SubsystemBase {
     encoder.setVelocityConversionFactor(2 * Math.PI);
     encoder.setZeroOffset(0);
   }
-
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run4
+    // This method will be called once per scheduler run
     double currentPose = encoder.getPosition();
+    double pidval = pid.calculate(currentPose, goal);
+    double ffval = ff.calculate(currentPose, 0);
+    motor.setVoltage(pidval + ffval);
 
-    switch(controlMode){
-      case POS:
-      {
-        double pidval = pid.calculate(currentPose, goal);
-        double ffval = ff.calculate(currentPose, 0);
-        motor.setVoltage(pidval + ffval);
-      }
-        break;
-      case VEL:
-      {
-        double ffval = ff.calculate(currentPose, goal);
-        motor.setVoltage(ffval);
-      }
-        break;
+    // check what range the arm is in and set the wrist accordingly
+    double armPose = Units.radiansToDegrees(arm.getPosition()) % 360; // I don't know what this is in i wish debugging was easier with this stupid dumb roboticriov2
+
+    if (WristState.UP.min <= armPose && armPose <= WristState.UP.max) {
+      setGoal(0);
+    } else if (WristState.LINEAR.min <= armPose && armPose <= WristState.LINEAR.max) {
+      setGoal(armPose);
+    } else if (WristState.FORWARD.min <= armPose && armPose <= WristState.FORWARD.max) {
+      setGoal(90);
     }
   }
 }
