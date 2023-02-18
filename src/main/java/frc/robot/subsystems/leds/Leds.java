e7ca84a10c91b5f1cdb987c123cfa3e4132bce31
Kevin Rock <150020151@gpschools.org>
2023-02-18 12:16:02 -0500

    changes

--- src/main/java/frc/robot/subsystems/leds/Leds.java
+++ src/main/java/frc/robot/subsystems/leds/Leds.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDS;
 
 public class Leds extends SubsystemBase {
-  /** Creates a new LEDS. */
+  /** Creates a new LEDS. d*/
   private AddressableLED ledStrip;
   private AddressableLEDSim ledSim;
 
