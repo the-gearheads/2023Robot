1cb9a94ebd45055151f1bf3507608eecea921fbf
Kevin Rock <150020151@gpschools.org>
2023-02-28 19:44:33 -0500

    changes

--- src/main/java/frc/robot/AutonPaths.java
+++ src/main/java/frc/robot/AutonPaths.java
@@ -27,7 +27,7 @@ public class AutonPaths {
   /* Places cone in high node, goes to GP1 to pick up a cube, places it in low hybrid node
    * ASSUMPTIONS: Cone preloaded, arm already inside robot, in inert pos, pos constants correct
    */
-  public static CommandBase getTestPlacePath(Subsystems s) {
+  public static CommandBase twoConePath(Subsystems s) {
     return new SequentialCommandGroup(
         // Move forward
         new ParallelCommandGroup(
@@ -61,6 +61,27 @@ public class AutonPaths {
     );
   }
 
+  public static CommandBase placeThenChargingStation(Subsystems s) {
+    return new SequentialCommandGroup(
+        // Move forward
+        // new ParallelCommandGroup(
+          new SetArmPose(s.arm, ArmPose.HIGH_NODE),
+          getCommandForPath("Inert_To_Start", true, Constants.AUTON.SLOW_CONSTRAINTS, s.swerve),
+        // ),
+
+        // place game piece
+        getPlaceConeCommand(s),
+
+        // Go get game piece
+        new ParallelCommandGroup(
+            getCommandForPath("Start_To_Outside_Charging_Station", false, Constants.AUTON.SLOW_CONSTRAINTS, s.swerve),
+            new SequentialCommandGroup( // Start moving the arm 1 second into the path following
+                new WaitCommand(1),
+                new SetArmPose(s.arm, ArmPose.INSIDE_ROBOT)
+              )
+        )
+    );
+  }
   /* Places a cone on the grid
    * ASSUPTIONS: Cone being held, arm in correct position, alt mode corresponds to setting wrist to 0deg, and default sets it to 90deg
    */
@@ -94,7 +115,7 @@ public class AutonPaths {
 
   public static Command getGrabberCloseCommand(Grabber grabber) {
     return new InstantCommand(() -> {
-      grabber.open();
+      grabber.close();
     }, grabber);
   }
 
