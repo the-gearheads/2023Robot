0bfa62981b8f806ad6769213d465a18bd74a8e43
person4268 <28717044+person4268@users.noreply.github.com>
2023-03-02 15:02:18 -0500

    sysid mechanism command works for arm now

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,6 +22,7 @@ import frc.robot.commands.arm.JoystickArmControl;
 import frc.robot.commands.arm.SetArmPose;
 import frc.robot.commands.arm.SetArmPose.ArmPose;
 import frc.robot.commands.drive.TeleopDrive;
+import frc.robot.commands.sysidcommand.SysidMechanismCommand;
 import frc.robot.commands.wrist.DefaultWristControl;
 import frc.robot.commands.wrist.AltWristControl;
 import frc.robot.controllers.Controllers;
@@ -32,6 +33,7 @@ import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.leds.Leds;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmSim;
+import frc.robot.subsystems.arm.Arm.ArmControlMode;
 import frc.robot.subsystems.wrist.Wrist;
 import frc.robot.subsystems.wrist.WristSim;
 import frc.robot.subsystems.drive.SwerveModule;
@@ -43,6 +45,7 @@ import frc.robot.subsystems.drive.gyro.GyroSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 /**
