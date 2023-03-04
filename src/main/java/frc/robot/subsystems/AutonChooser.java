b6215afdde5f95a43ed3304f9c8622b7809215ac
AnotherUser12 <150020151@gpschools.org>
2023-03-03 20:04:37 -0500

    fixed all auton paths

--- src/main/java/frc/robot/subsystems/AutonChooser.java
+++ src/main/java/frc/robot/subsystems/AutonChooser.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import java.util.HashMap;
 import java.util.Map;
+import javax.naming.AuthenticationNotSupportedException;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -76,15 +77,23 @@ public class AutonChooser {
   public void initializeAutons() {//Here we define auton choices
     Subsystems s = new Subsystems(swerve, wrist, arm, grabber);
     //Left Most Cone Node -> Left Most Game Piece -> Left Most Cone Node -> Charging Station 
-    Command testPath = AutonPaths.InertN1TwoConePath(s);
-    autons.put("Left, 2 Cones", testPath);
+    Command InertN1TwoConePath = AutonPaths.InertN1TwoConePath(s);
+    Command InertN1GrabThenDock = AutonPaths.InertN1GrabThenDock(s);
+    Command InertN1PlaceThenDock = AutonPaths.InertN1PlaceThenDock(s);
+    Command InertN9TwoConePath = AutonPaths.InertN9TwoConePath(s);
+    Command InertN9GrabThenDock = AutonPaths.InertN9GrabThenDock(s);
+    Command InertN9PlaceThenDock = AutonPaths.InertN9PlaceThenDock(s);
 
-    Command placeThenChargingStation = AutonPaths.InertN1PlaceThenDock(s);
-    autons.put("Left, place cone, charging station", placeThenChargingStation);
+    autons.put("N1 TwoCone", InertN1TwoConePath);
+    autons.put("N1 Grab Then Dock", InertN1GrabThenDock);
+    autons.put("N1 Place Then Dock", InertN1PlaceThenDock);
+    autons.put("N9 TwoCone", InertN9TwoConePath);
+    autons.put("N9 Grab Then Dock", InertN9GrabThenDock);
+    autons.put("N9 Place Then Dock", InertN9PlaceThenDock);
 
-    //Forward 1 Meter
-    Command debugForward = AutonPaths.getCommandForPath("Debug_Forward", true, AUTON.SLOW_CONSTRAINTS, swerve);
-    autons.put("Debug Forward", debugForward);
+    // //Forward 1 Meter
+    // Command debugForward = AutonPaths.getCommandForPath("Debug_Forward", true, AUTON.SLOW_CONSTRAINTS, swerve);
+    // autons.put("Debug Forward", debugForward);
 
     Command autoBalance = new AutoBalance(swerve);
     autons.put("Auto Balance", autoBalance);
