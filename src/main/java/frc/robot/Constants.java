618f6c9db19d2da0a9a54c23f9815ac597c0241b
Kevin Rock <150020151@gpschools.org>
2023-03-04 13:03:13 -0500

    wrist wrapping code bad

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -66,7 +66,7 @@ public class Constants extends AnnotatedClass {
     public static final double CHASSIS_Y = 0;
 
     public static final double ARM_REDUCTION = 200;
-    public static final double WRIST_REDUCTION = 80;
+    public static final double WRIST_REDUCTION = 80; // DONT CHANGE THIS VALUE!!!!
 
     public static final double ARM_MASS = Units.lbsToKilograms(6);
     public static final double WRIST_MASS = Units.lbsToKilograms(0.5);//should be 3.5 but whatev
