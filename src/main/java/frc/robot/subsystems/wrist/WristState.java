b6a2981d7ecc2765c4517f6c30887daaadf2fbb5
Kevin Rock <150020151@gpschools.org>
2023-03-04 15:24:28 -0500

    wrist

--- src/main/java/frc/robot/subsystems/wrist/WristState.java
+++ src/main/java/frc/robot/subsystems/wrist/WristState.java
@@ -7,8 +7,8 @@ public enum WristState {
   //These values are ALL WRONG
   //arm 0 deg is positive x axis
   // format:off
-  FEEDER_STATION(ARM.MIN_ANGLE, -170, -90), TRANSITION(-170, -140, 0), INSIDE_ROBOT(-140, -70, 90,
-      WristStateType.OVERRIDE), RIGHT(-70, ARM.MAX_ANGLE, 45),
+  FEEDER_STATION(ARM.MIN_ANGLE, -170, -110.7), TRANSITION(-170, -140, 0), INSIDE_ROBOT(-140, -70, 90,
+      WristStateType.OVERRIDE), RIGHT(-70, ARM.MAX_ANGLE, 90),
 
   GROUND_PICK_UP(-66, -50, -90, WristStateType.ALT), PLACE_ON_NODE(-50, ARM.MAX_ANGLE, 0, WristStateType.ALT);
   // format:on
