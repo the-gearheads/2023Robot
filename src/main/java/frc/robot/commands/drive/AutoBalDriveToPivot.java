83b6e93c047d1d3e3319ea4c1ebfa434ab59fa9b
Ceticx0 <Gavinoperry9@gmail.com>
2023-03-19 15:27:16 -0400

    made it stop moving at end of pivot cmd

--- src/main/java/frc/robot/commands/drive/AutoBalDriveToPivot.java
+++ src/main/java/frc/robot/commands/drive/AutoBalDriveToPivot.java
@@ -28,7 +28,9 @@ public class AutoBalDriveToPivot extends CommandBase {
 
     // Called once the commak nd ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+      swerve.drive(new ChassisSpeeds(0, 0, 0));
+    }
 
     // Returns true when the command should end.
     @Override
