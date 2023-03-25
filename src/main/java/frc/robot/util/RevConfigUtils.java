0f0fe0e3d46ccfe420da3c72ace545cdeb7cebf8
person4268 <28717044+person4268@users.noreply.github.com>
2023-03-25 15:40:04 -0400

    retry for arm and wrist?

--- src/main/java/frc/robot/util/RevConfigUtils.java
+++ src/main/java/frc/robot/util/RevConfigUtils.java
@@ -7,36 +7,40 @@ import com.revrobotics.REVLibError;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class RevConfigUtils {
-  
+
   /**
    * Runs a rev motor controller configure function with retry support.
+   * 
    * @param configureFunc Function that returns an ArrayList of REVLibErrors
    * @param subsystemName Name of subsystem (or motor)
    * @param maxRetries Max number of retries (default 5)
    */
   public static void configure(Supplier<ArrayList<REVLibError>> configureFunc, String subsystemName, int maxRetries) {
     /* Loop for maxRetries */
-    for(int retries = 0; retries < maxRetries; retries++) {
+    for (int retries = 0; retries < maxRetries; retries++) {
       var errors = configureFunc.get();
       boolean reconfigure = false;
       /* Loop over all errors */
-      for(int i = 0; i < errors.size(); i++) {
+      for (int i = 0; i < errors.size(); i++) {
         if (errors.get(i) != REVLibError.kOk) {
           reconfigure = true;
-          DriverStation.reportError("Configure function error " + errors.get(i).name() + " at i=" + i + "in subsystem " + subsystemName
-                                  + "(retry " + retries + "/" + maxRetries + ")", true);
+          DriverStation.reportError("Configure function error " + errors.get(i).name() + " at i=" + i + "in subsystem "
+              + subsystemName + "(retry " + retries + "/" + maxRetries + ")", true);
 
           /* Not super useful or detailed, i just want this to also be in NT and logs */
           Logger.getInstance().recordOutput("Errors/" + subsystemName + errors.get(i).name(), true);
         }
       }
       /* Don't retry if no errors */
-      if (!reconfigure) {break;}
+      if (!reconfigure) {
+        break;
+      }
     }
   }
 
   /**
    * Runs a rev motor controller configure function with retry support.
+   * 
    * @param configureFunc Function that returns an ArrayList of REVLibErrors
    * @param subsystemName Name of subsystem (or motor)
    */
