ba0085cadcfae27ca139130517dd29c91fe590a4
Kevin Rock <150020151@gpschools.org>
2023-04-18 10:05:18 -0400

    removal of unnecessary execute method in WaitForDriveAwayCommand

--- src/main/java/frc/robot/commands/WaitForDriveAwayCommand.java
+++ src/main/java/frc/robot/commands/WaitForDriveAwayCommand.java
@@ -25,11 +25,6 @@ public class WaitForDriveAwayCommand extends CommandBase {
       leds.setState(LedState.FLASH_GREEN);
     }
   
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-    }
-  
     // Called once the commak nd ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
